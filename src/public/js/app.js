dba39a5b82c11cb384f9422d76cc97b617dd20ee
nohkwanok <cloq@kakao.com>
2022-02-26 22:49:39 +0900

    #3.0 - # 3.2 getAudioTracks, getVideoTracks 를 사용해서 display 안의 카메라, 마이크를 제어함 navigator.mediaDevices.enumerateDevices 를 통해 미디어 디바이스들을 가져올 수 있음 카메라 id를 통해서 장치 내의 카메라들을 스위칭할 수 있음

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,85 +1,93 @@
 const socket = io()
 
-const welcome = document.getElementById("welcome")
-const form = welcome.querySelector("form")
-const room = document.getElementById("room")
+const myFace = document.getElementById("myFace")
+const muteButton = document.getElementById("mute")
+const cameraButton = document.getElementById("camera")
+const cameraSelect = document.getElementById("cameras")
 
-room.hidden = true
+let myStream
+let muted = false
+let cameraOff = false
 
-let roomName
-
-function addMessage(message)
-{
-    const ul = room.querySelector("ul")
-    const li = document.createElement("li")
-    li.innerText = message
-    ul.appendChild(li)
-
-}
-
-function showRoom() {
-    welcome.hidden = true
-    room.hidden = false
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName}`
-    const msgForm = room.querySelector("#msg")
-    const nameForm = room.querySelector("#name")
-    msgForm.addEventListener("submit", handleMessageSubmit)
-    nameForm.addEventListener("submit", handleNickNameSubmit)
-}
-
-function handleRoomSubmit(event) {
-    event.preventDefault()
-    const input = form.querySelector("input")
-    socket.emit("enter_room", input.value, showRoom)
-    roomName = input.value
-    input.value = ""
-}
-
-function handleMessageSubmit(event) {
-    event.preventDefault()
-    const input = room.querySelector("#msg input")
-    const value = input.value
-    socket.emit("new_message", value, roomName, () => {
-        addMessage(`나: ${value}`)
-    })
-    input.value = ""
-}
-
-function handleNickNameSubmit(event) {
-    event.preventDefault()
-    const input = room.querySelector("#name input")
-    socket.emit("nickname", input.value)
-}
-
-form.addEventListener("submit", handleRoomSubmit)
-
-
-socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${user} 들어와따!`)
-})
-
-socket.on("bye", (user, newCount) => {
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${user} 나갔다!`)
-})
-
-socket.on("new_message", addMessage)
-
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul")
-    roomList.innerHTML = ""
-    if (rooms.length === 0) {
-        return
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices()
+        const cameras = devices.filter(device => device.kind === "videoinput")
+        const currentCamera = myStream.getVideoTracks()[0]
+        cameras.forEach(camera => {
+            const option = document.createElement("option")
+            option.value = camera.deviceId
+            option.innerText = camera.label
+            if (currentCamera.label === camera.label) {
+                option.selected = true
+            }
+            cameraSelect.appendChild(option)
+        })
+    } catch (e) {
+        console.log(e)
     }
-    rooms.forEach(room => {
-        const li = document.createElement("li")
-        li.innerText = room
-        roomList.append(li)
-    })
+}
+
+async function getMedia(deviceId) {
+    const initConstraints = {
+        audio: true,
+        video: {facingMode: "user"}
+    }
+
+    const cameraConstraints = {
+        audio: true,
+        video: {deviceId: {exact: deviceId}},
+        // video: { deviceId },
+    }
+
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initConstraints
+        )
+        myFace.srcObject = myStream
+        if (!deviceId) {
+            await getCameras()
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+function handleMuteClick() {
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled))
+    if (!muted) {
+        muteButton.innerText = "소리 켜기"
+        muted = true
+    } else {
+        muteButton.innerText = "음소거"
+        muted = false
+    }
+}
+
+function handleCameraClick() {
+    myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = !track.enabled))
+    if (cameraOff) {
+        cameraButton.innerText = "카메라 끄기"
+        cameraOff = false
+    } else {
+        cameraButton.innerText = "카메라 켜기"
+        cameraOff = true
+    }
+}
+
+getMedia().then(() => {
+    console.log("Good")
 })
-// 밑에거랑 똑같음
-// socket.on("room_change", (msg) => console.log(msg))
+
+async function handleCameraChange() {
+    await getMedia(cameraSelect.value)
+}
+
+muteButton.addEventListener("click", handleMuteClick)
+cameraButton.addEventListener("click", handleCameraClick)
+
+cameraSelect.addEventListener("input", handleCameraChange)
